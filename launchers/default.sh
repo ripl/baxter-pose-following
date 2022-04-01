265d2a2bc491d0f008c405d3c6b82d22c7212fc0
Andrea F. Daniele <afdaniele@ttic.edu>
2022-04-01 15:21:56 -0500

    improved

--- launchers/default.sh
+++ launchers/default.sh
@@ -7,7 +7,14 @@ source /cpk/environment.sh
 
 # launching app
 source ${CPK_CODE_DIR}/devel/setup.bash
-cpk-exec roslaunch realsense2_camera rs_camera.launch align_depth:=true
+#cpk-exec roslaunch realsense2_camera rs_camera.launch \
+#    depth_width:=424 \
+#    depth_height:=240 \
+#    depth_fps:=15 \
+#    color_width:=424 \
+#    color_height:=240 \
+#    color_fps:=15 \
+#    align_depth:=true
 cpk-exec rosrun trt_pose pose_estimator.py
 cpk-exec rosrun trt_pose pose_tracker.py
 rosrun trt_pose pose_visualizer.py
---
+++ launchers/realsense.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /cpk/environment.sh
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# launching app
+source ${CPK_CODE_DIR}/devel/setup.bash
+roslaunch realsense2_camera rs_camera.launch \
+    depth_width:=424 \
+    depth_height:=240 \
+    depth_fps:=15 \
+    color_width:=424 \
+    color_height:=240 \
+    color_fps:=15 \
+    align_depth:=true
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
