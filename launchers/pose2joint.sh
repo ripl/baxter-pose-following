5fc1113639aa619340bb73c2ab1c82f09b01f6ef
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-04-07 18:26:22 -0500

    add baxter_moveit_config and rviz launch file

---
+++ launchers/pose2joint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /cpk/environment.sh
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# launching app
+source ${CPK_CODE_DIR}/devel/setup.bash
+# cpk-exec rosrun trt_pose pose_tracker.py
+# cpk-exec rosrun trt_pose pose_visualizer.py
+# rosrun trt_pose pose_estimator.py
+rosrun pose2movement pose2joint.py
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
