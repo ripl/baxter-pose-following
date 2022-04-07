2e85043697d0d5b96959923665d7e4914a23b2f4
Shengjie Lin <StArchon@outlook.com>
2022-04-07 14:28:35 -0500

    update

--- launchers/movement_by_angles.sh
+++ launchers/movement_by_angles.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+source /cpk/environment.sh
+
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
 
 # launching app
 source ${CPK_CODE_DIR}/devel/setup.bash
-rosrun pose2movement random_movement.py
+cpk-exec rosrun pose2movement pose2joint.py
+rosrun pose2movement coords_viz.py
 
 
 # ----------------------------------------------------------------------------
