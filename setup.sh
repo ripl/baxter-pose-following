ec08c4ae37f886f8069e8906521adb4e067c814d
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-04-07 23:56:05 -0500

    tiny calibration

--- setup.sh
+++ setup.sh
@@ -11,5 +11,16 @@ export PYTHONPATH="${PYTHONPATH//python3.8/python3.6}"
 
 # configure ROS network
 export ROS_MASTER_URI=http://10.0.0.10:11311
-export ROS_HOSTNAME=backpack.local
+# export ROS_HOSTNAME=backpack.local
+
 # export ROS_HOSTNAME=slin-HP-Spectre.local
+export ROS_HOSTNAME=takumasus.local
+# export ROS_IP=10.0.0.11  # backpack.local
+# export ROS_HOSTNAME=slin-HP-Spectre.local
+
+# pinocchio
+export PATH="/opt/openrobots/bin:$PATH"
+export PKG_CONFIG_PATH="/opt/openrobots/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="/opt/openrobots/lib:$LD_LIBRARY_PATH"
+export PYTHONPATH="/opt/openrobots/lib/python3.8/site-packages:$PYTHONPATH"
+export CMAKE_PREFIX_PATH="/opt/openrobots:$CMAKE_PREFIX_PATH"
