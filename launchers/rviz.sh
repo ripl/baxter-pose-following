7ffa7fd3df16a2b8bba7f83f59ccf5950a4688e6
Takuma Yoneda <takuma-yoneda@users.noreply.github.com>
2022-04-07 23:23:39 -0500

    add launch files

---
+++ launchers/rviz.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+source /cpk/environment.sh
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# launching app
+source ${CPK_CODE_DIR}/devel/setup.bash
+# cpk-exec rosrun trt_pose pose_tracker.py
+# cpk-exec rosrun trt_pose pose_visualizer.py
+# rosrun trt_pose pose_estimator.py
+roslaunch baxter_moveit_config baxter_grippers.launch
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
