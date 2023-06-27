641247fac1990473d8d8b2e9b50dcc74d551c94a
Shengjie Lin <StArchon@outlook.com>
2023-06-26 22:46:54 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # NOTE: this setup script will be executed right before the launcher file inside the container,
 #       use it to configure your environment.
 
-source ${CPK_CODE_DIR}/devel/setup.bash
+. ${CPK_CODE_DIR}/devel/setup.bash
 
 export ROS_HOSTNAME=$(hostname).local
 export ROS_MASTER_URI=http://011504P0002.local:11311
